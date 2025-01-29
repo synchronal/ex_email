e19a8c57df7b27cbfe83ad254ecb4ad10ae01c66
Eric Saxby <sax@livinginthepast.org>
2025-01-29 10:51:57 -0800

    Mark abnf file as an external resource

--- lib/ex_email/parser.ex
+++ lib/ex_email/parser.ex
@@ -1,6 +1,8 @@
 defmodule ExEmail.Parser do
   @moduledoc false
 
+  @external_resource "priv/parser/rfc5321.abnf"
+
   use AbnfParsec,
     abnf_file: "priv/parser/rfc5321.abnf",
     parse: :mailbox
