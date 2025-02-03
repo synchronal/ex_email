68d433a1dee3aa883909e1b7f95f3caa57d84ec0
Eric Saxby <sax@livinginthepast.org>
2025-02-03 10:52:58 -0800

    Add docs

--- lib/ex_email.ex
+++ lib/ex_email.ex
@@ -1,6 +1,26 @@
 defmodule ExEmail do
   @moduledoc """
-  A pure-Elixir reimplementation of `:email_validator`.
+  `ExEmail` implements a parser and validator for email addresses according to the
+  ABNF rules defined in the following RFCs:
+
+  - [RFC5321 - Simple Mail Transfer Protocol](https://tools.ietf.org/rfc/rfc5321.txt)
+  - [RFC6531 - SMTP Extension for Internationalized](https://tools.ietf.org/rfc/rfc6531.txt)
+  - [RFC6532 - Internationalized Email Headers](https://tools.ietf.org/rfc/rfc6532.txt)
+
+  When parsing emails addressed to IP addresses, the domain part is parsed via the rules
+  defined in
+  [RFC3986: Uniform Resource Identifier (URI): Generic Syntax](https://tools.ietf.org/rfc/rfc3986.txt).
+
+  ## Notes
+
+  - `ExEmail` is a reimplementation of [`:email_validator`](https://hex.pm/packages/email_validator)
+    in Elixir. It's a fantastic library.
+  - ABNF rules are compiled using `AbnfParsec`. Because it's built on top of `NimbleParsec`,
+    the ABNF shipped with this library differs from the relevant RFCs in specific ways. ABNF
+    assumes that when a rule defines multiple choices, that each choice can be attempted, and
+    then backtracked to the last matching rule; `NimbleParsec` does not support backtracking,
+    so rules must be defined such that the most appropriate rule is most likely to be matched
+    first.
   """
 
   alias ExEmail.Error
@@ -9,8 +29,28 @@ defmodule ExEmail do
   @max_local 64
   @max_length @max_domain + @max_local + 1
 
-  @type t() :: {String.t(), String.t()}
+  @typedoc """
+  A tuple of the local and domain parts of an email address; parsing the address
+  `alice@example.com` returns `{"alice", "example.com"}`.
+  """
+  @type t() :: {local_part(), domain_part()}
+  @type local_part() :: String.t()
+  @type domain_part() :: String.t()
 
+  @doc """
+  Validates the format of an email address, returning either `:ok` or a
+  tuple of `{:error, ExEmail.Error.t()}`.
+
+  ## Examples
+
+  ``` elixir
+  iex> ExEmail.validate("a@example.com")
+  :ok
+
+  iex> ExEmail.validate("@example.com")
+  {:error, ExEmail.Error.new("parse error", "@example.com")}
+  ```
+  """
   @spec validate(String.t()) :: :ok | {:error, Error.t()}
   def validate(address) do
     case parse(address) do
@@ -19,6 +59,20 @@ defmodule ExEmail do
     end
   end
 
+  @doc """
+  Parses an email address into a tuple of `{:ok, {local_part, domain_part}}`, or
+  `{:error, ExEmail.Error.t()}`.
+
+  ## Examples
+
+  ``` elixir
+  iex> ExEmail.parse("a@example.com")
+  {:ok, {"a", "example.com"}}
+
+  iex> ExEmail.parse("@example.com")
+  {:error, ExEmail.Error.new("parse error", "@example.com")}
+  ```
+  """
   @spec parse(String.t()) :: {:ok, t()} | {:error, Error.t()}
   def parse(address) when is_binary(address) do
     case parse_address(address) do
