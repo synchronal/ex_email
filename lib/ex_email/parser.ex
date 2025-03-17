7391f02dc6f4e9d38b5320d5ee15975f7e2fabdf
Eric Saxby <sax@livinginthepast.org>
2025-03-17 11:12:10 -0700

    Use code priv_dir to declare external resource

--- lib/ex_email/parser.ex
+++ lib/ex_email/parser.ex
@@ -1,7 +1,7 @@
 defmodule ExEmail.Parser do
   @moduledoc false
 
-  @external_resource "priv/parser/rfc5321.abnf"
+  @external_resource Path.join(:code.priv_dir(:ex_email), "parser/rfc5321.abnf")
 
   use AbnfParsec,
     abnf_file: "priv/parser/rfc5321.abnf",
